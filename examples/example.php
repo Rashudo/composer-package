c27eede86799da9d8dd9156163acba88f5b964db
Rashdor_Laptop <rashdor@gmail.com>
2021-05-13 11:16:42 +0300

    init commit

---
+++ examples/example.php
@@ -0,0 +1,6 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use sgaponov\HelloWorld\HelloWorld;
+
+echo HelloWorld::index();
---
+++ src/HelloWorld.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace sgaponov\HelloWorld;
+
+/**
+ * Class HelloWorld
+ * @package sgaponov\HelloWorld
+ */
+class HelloWorld
+{
+    /**
+     * @return string
+     */
+    public static function index(): string
+    {
+        return "Hello World";
+    }
+}
